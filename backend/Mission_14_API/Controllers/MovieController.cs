46363728c16b369c3c7aff020340ac6dc0145e0a
jel14t <jay.lowry350@gmail.com>
2023-04-04 12:11:45 -0600

    Mission 14 part 2

---
+++ backend/Mission_14_API/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_14_API.Data;
+
+namespace Mission_14_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+        private readonly MovieContext context;
+
+        public MovieController(MovieContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ backend/Mission_14_API/Migrations/20230404033500_Inital.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission_14_API.Migrations
+{
+    public partial class Inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Category = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Title = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Year = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Director = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Rating = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Edited = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    LentTo = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Notes = table.Column<byte[]>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
