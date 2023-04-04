8b88a9e4701c431db0f5880e738efa9e534d9052
jel14t <jay.lowry350@gmail.com>
2023-04-04 12:09:17 -0600

    Mission 14

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { useState } from 'react';
-import data from '../MovieData.json';
+import { useState, useEffect } from 'react';
 
-const MDS = data.MovieData;
+export type Movie = {
+  movieId: number;
+  category: string;
+  title: string;
+  year: number;
+  director: string;
+  rating: string;
+  edited: boolean;
+  lentTo: string;
+  notes: string;
+};
 
 export function MovieList() {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const rsp = await fetch('https://localhost:4000/api/movie');
+      const temp = await rsp.json();
+      setMovieData(temp);
+    };
+    fetchMovies();
+  }, []);
+
+  const editedMovies = movieData.filter((movie) => movie.edited);
+  const sortedMovies = editedMovies.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <>
       <header className="bg-dark py-5">
@@ -17,29 +48,32 @@ export function MovieList() {
         </div>
       </header>
       <div>
-        <h3></h3>
-      </div>
-      <div>
         <table className="table">
-          <thead>
+          <thead key="thead">
             <tr>
+              <th>ID</th>
               <th>Title</th>
               <th>Year</th>
               <th>Director</th>
               <th>Rating</th>
               <th>Category</th>
               <th>Edited?</th>
+              <th>Lent To</th>
+              <th>Notes</th>
             </tr>
           </thead>
-          <tbody>
-            {MDS.map((m: any) => (
-              <tr>
-                <th>{m.Title}</th>
-                <th>{m.Year}</th>
-                <th>{m.Director}</th>
-                <th>{m.Rating}</th>
-                <th>{m.Category}</th>
-                <th>{m.Edited}</th>
+          <tbody key="tbody">
+            {sortedMovies.map((m: any) => (
+              <tr key={m.movieId}>
+                <th>{m.movieId}</th>
+                <th>{m.title}</th>
+                <th>{m.year}</th>
+                <th>{m.director}</th>
+                <th>{m.rating}</th>
+                <th>{m.category}</th>
+                <th>{m.edited ? 'Yes' : 'No'}</th>
+                <th>{m.lentTo}</th>
+                <th>{m.notes}</th>
               </tr>
             ))}
           </tbody>
@@ -48,8 +82,3 @@ export function MovieList() {
     </>
   );
 }
-
-// function MovieList() {
-
-//
-// }
